0a871bcbb07995a68e905294afff50b60b0c9b47
APAC\CJA2KOR <cja2kor@bosch.com>
2022-07-08 17:23:50 +0530

    changes as per rquirement

--- checker.cpp
+++ checker.cpp
@@ -3,17 +3,15 @@
 #include "checker.hpp"
 using namespace std;
 using namespace BatteryStatus;
-bool CheckStstus(int min,int max,float value, std::string str)
-{
+void printError(std::string str){
+	cout<<str << " out of range! "<<endl;
+}
+bool ChecksStatus(int min,int max,float value, std::string str){
 	if(value < min || value > max) {
     printError(str);
     return false;
 	}
 	return true;
-}
-void printError(std::string str)
-{
-	cout<<str << " out of range! "<<endl;
 }
 bool Battery::batteryIsOk(std::vector<float>ParametersofBatteryStatus) {
 	int i = 0;
@@ -25,16 +23,13 @@ bool Battery::batteryIsOk(std::vector<float>ParametersofBatteryStatus) {
     }
 	return true;
 }
-bool Battery::batteryTemperatureIsOk(float temperature)
-{
-	return CheckStstus(0,45,temperature,"Temperature");
+bool Battery::batteryTemperatureIsOk(float temperature){
+	return ChecksStatus(0,45,temperature,"Temperature");
 }
-bool Battery::batterysocIsOk(float soc)
-{
-	 return CheckStstus(20,80,soc,"State of Charge");
+bool Battery::batterysocIsOk(float soc){
+	 return ChecksStatus(20,80,soc,"State of Charge");
 }
-bool Battery::batterychargeRateIsOk(float chargeRate)
-{
+bool Battery::batterychargeRateIsOk(float chargeRate){
 	if(chargeRate > 0.8) {
     printError("Charge Rate");
     return false;
