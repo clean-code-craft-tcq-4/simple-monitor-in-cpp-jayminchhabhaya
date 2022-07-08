3691855dbbca7e2050a4f0eb76fb3561d754c0b2
APAC\CJA2KOR <cja2kor@bosch.com>
2022-07-08 17:21:54 +0530

    changes as per rquirement

--- checker.cpp
+++ checker.cpp
@@ -3,36 +3,40 @@
 #include "checker.hpp"
 using namespace std;
 using namespace BatteryStatus;
+bool CheckStstus(int min,int max,float value, std::string str)
+{
+	if(value < min || value > max) {
+    printError(str);
+    return false;
+	}
+	return true;
+}
+void printError(std::string str)
+{
+	cout<<str << " out of range! "<<endl;
+}
 bool Battery::batteryIsOk(std::vector<float>ParametersofBatteryStatus) {
 	int i = 0;
 	for (const auto& funptr : functptr) {
-        if(!funptr(ParametersofBatteryStatus[i++]))
-		{
-		 return false;
-		}
+      if(!funptr(ParametersofBatteryStatus[i++]))
+	  {
+		return false;
+	  }
     }
 	return true;
 }
 bool Battery::batteryTemperatureIsOk(float temperature)
 {
-	if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-	}
-	return true;
+	return CheckStstus(0,45,temperature,"Temperature");
 }
 bool Battery::batterysocIsOk(float soc)
 {
-	if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  }
-  return true;
+	 return CheckStstus(20,80,soc,"State of Charge");
 }
 bool Battery::batterychargeRateIsOk(float chargeRate)
 {
 	if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
+    printError("Charge Rate");
     return false;
   }
   return true;
